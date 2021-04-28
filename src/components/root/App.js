417b3e605b87100d6757bf832346aa668dab4f8b
Nuru Mohamed <nurucs@users.noreply.github.com>
2021-04-28 12:50:21 +0300

    (style) App component structured to change its bg-color based on state

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,18 +1,19 @@
-import Home from '../home/home.js';
+import Home from '../home/home';
+import SearchResult from '../searchResult/search-result';
 import styled from 'styled-components';
 
+const state = "search-result";
 const Wrapper = styled.div`
   font-family: 'Roboto';
-  height: 100%;
-
-  
-    
+  height: 100%; 
+  background-color: ${ () => state=="search-result"? "rgba(0,0,0,0.1)" : "white"};
 `;
 
 function App() {
   return (
     <Wrapper className="App">
-      <Home/>
+      {/* <Home/> */}
+      <SearchResult/>
     </Wrapper>
   );
 }
