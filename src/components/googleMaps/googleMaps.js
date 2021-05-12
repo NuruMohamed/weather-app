a1183ef484125050c3d1b6c825c8bbc8ac70b31c
Nuru Mohamed <nurucs@users.noreply.github.com>
2021-05-12 14:22:48 +0300

    (fix) map height made responsive

--- src/components/googleMaps/googleMaps.js
+++ src/components/googleMaps/googleMaps.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 let lon = 38.6380575;
 let lat = 8.9630978;
 
 
+
 const Wrapper = styled.div`
     width: 100%;    
-    /* height: 5%; */
+    border: 1px solid red; 
+
+    /* height: 65%; */
     img {
         width: 100%;
         border-radius: 10px;
@@ -18,7 +22,8 @@ const Wrapper = styled.div`
 `;
 
 function GoogleMaps({coord: { lat, lon}}) {
-    const url = `https://maps.googleapis.com/maps/api/staticmap?size=900x120&zoom=10&markers=color:red%7C${lat},${lon}&key=AIzaSyC9z-i2g5RgCVF80eUbeCZsrzbnN0csu8M`;
+    let mapHeight = useMediaQuery('(max-width: 600px)');
+    const url = `https://maps.googleapis.com/maps/api/staticmap?size=900x${mapHeight? 180 : 130}&zoom=10&markers=color:red%7C${lat},${lon}&key=AIzaSyC9z-i2g5RgCVF80eUbeCZsrzbnN0csu8M`;
 
     return (
         <Wrapper>
